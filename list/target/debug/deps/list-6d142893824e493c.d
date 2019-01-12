5a86662766260cfeeb82ce4d9ccb49f6ac7b57a5
Austin <github@shoecraft.xyz>
2019-01-12 12:59:48 -0500

    list example

---
+++ list/target/debug/deps/list-6d142893824e493c.d
@@ -0,0 +1,5 @@
+/Users/austin/Development/rust-by-example/list/target/debug/deps/list-6d142893824e493c: src/main.rs
+
+/Users/austin/Development/rust-by-example/list/target/debug/deps/list-6d142893824e493c.d: src/main.rs
+
+src/main.rs:
---
+++ list/target/debug/list.d
@@ -0,0 +1,1 @@
+/Users/austin/Development/rust-by-example/list/target/debug/list.dSYM: /Users/austin/Development/rust-by-example/list/src/main.rs
