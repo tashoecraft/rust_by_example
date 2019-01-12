86bf7fe863ef864a4f8216d1e8058f0f120bd124
Austin <github@shoecraft.xyz>
2019-01-12 12:51:20 -0500

    initial

---
+++ display/target/debug/deps/display-d7305941a2e3de19.d
@@ -0,0 +1,5 @@
+/Users/austin/Development/rust-by-example/display/target/debug/deps/display-d7305941a2e3de19: src/main.rs
+
+/Users/austin/Development/rust-by-example/display/target/debug/deps/display-d7305941a2e3de19.d: src/main.rs
+
+src/main.rs:
---
+++ display/target/debug/display.d
@@ -0,0 +1,1 @@
+/Users/austin/Development/rust-by-example/display/target/debug/display.dSYM: /Users/austin/Development/rust-by-example/display/src/main.rs
