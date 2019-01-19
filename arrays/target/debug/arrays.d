780f30aa2014d3106543fc22a97c1689e663efee
Austin <github@shoecraft.xyz>
2019-01-19 14:12:27 -0500

    fixed array index issue

---
+++ arrays/target/debug/arrays.d
@@ -0,0 +1,1 @@
+/Users/austin/Development/rust-by-example/arrays/target/debug/arrays.dSYM: /Users/austin/Development/rust-by-example/arrays/src/main.rs
---
+++ arrays/target/debug/deps/arrays-093582115e4e0bc7.d
@@ -0,0 +1,5 @@
+/Users/austin/Development/rust-by-example/arrays/target/debug/deps/arrays-093582115e4e0bc7: src/main.rs
+
+/Users/austin/Development/rust-by-example/arrays/target/debug/deps/arrays-093582115e4e0bc7.d: src/main.rs
+
+src/main.rs:
